b384ff4b95f24307b5b7bcf49e8d3b22aab81653
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-08 19:47:16 +0200

    chore: removed 'q'

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import {test, expect} from '@playwright/test';
 test('check text on page', async ({page}) => {
   await page.goto('http://localhost:3000/');
 
-  await expect(page.getByText('Manage your pet daycarepets with ease')).toBeVisible()
+  await expect(page.getByText('Manage your pet daycarepets 1 with ease')).toBeVisible()
 })
