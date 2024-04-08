19428ee69af81fef75397418c321adb7671b9a06
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-08 16:57:10 +0200

    chore: fixed test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
     title: 'Welcome to pet-software',
-    description: 'Take care of your pets with pet-software',
+    description: 'Take care of your pets with pet-software!',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 {children}
                 <div
                     className="absolute bottom-8 right-8 flex
-             h-6 w-12 items-center justify-center rounded-xl border-2 
+             h-6 min-w-12 items-center justify-center rounded-xl border-2
              border-black bg-slate-400/50 p-4"
                 >
                     ENV: {nodeEnv}; Version: {version}
