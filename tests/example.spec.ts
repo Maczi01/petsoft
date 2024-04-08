c6ba1e3ef8c0c71bb7271f1696e4aa8920ed4ed4
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-08 20:00:28 +0200

    chore: updated tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test';
 
 test('check text on page', async ({page}) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
 
   await expect(page.getByText('Manage your pet daycarepets 1 with ease')).toBeVisible()
 })
