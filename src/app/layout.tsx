75e0efee5c44f2ff92f7dafa1f63ce368dbd90d3
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-08 13:24:32 +0200

    chore: update prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <body className={`${inter.className} min-h-screen bg-[#E5E8EC] text-sm text-zinc-900`}>
                 {children}
                 <div
-                    className="w-12 absolute bottom-8 right-8
-             flex h-6 items-center justify-center rounded-xl border-2 
+                    className="absolute bottom-8 right-8 flex
+             h-6 w-12 items-center justify-center rounded-xl border-2 
              border-black bg-slate-400/50 p-4"
                 >
                     ENV: {nodeEnv}; Version: {version}
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import logo from '../../public/logo.svg';
 import Link from 'next/link';
 import Image from 'next/image';
