7a949785c3929d995e60c21f9dcc96250124c708
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-08 20:31:38 +0200

    chore: fixed test url

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import {test, expect} from '@playwright/test';
 test('check text on page', async ({page}) => {
   await page.goto('/');
   console.log('process,', process.env.PLAYWRIGHT_TEST_BASE_URL)
-  await expect(page.getByText('Manage your pet daycarepets with ease')).toBeVisible()
+  await expect(page.getByText('Manage your pet day care with ease')).toBeVisible()
 })
