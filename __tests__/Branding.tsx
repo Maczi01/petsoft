42d3237002ee06b88c41c65b7d18e48d04a8fc1b
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-08 13:51:40 +0200

    chore: added jest test

---
+++ __tests__/Branding.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'; // For the extended matchers like .toBeInTheDocument()
+import { Branding } from '../src/components/branding';
+describe('Branding Component', () => {
+
+    test('renders the H1 component with correct text', () => {
+        render(<Branding />);
+        const h1Element = screen.getByRole('heading', { level: 1 });
+        expect(h1Element).toHaveTextContent('Pet Soft');
+    });
+
+    test('paragraph contains correct class and text', () => {
+        render(<Branding />);
+        const paragraphElement = screen.getByText('Manage your pets with ease', { selector: 'p' });
+        expect(paragraphElement).toBeInTheDocument();
+        expect(paragraphElement).toHaveClass('text-lg opacity-80');
+    });
+});
