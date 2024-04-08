ef5d6b45f21fc1487fe77689c341f82efb750ea1
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-08 20:15:39 +0200

    chore: fixed test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,6 @@ import {test, expect} from '@playwright/test';
 
 test('check text on page', async ({page}) => {
   await page.goto('/');
-
+  console.log('process,', process.env.PLAYWRIGHT_TEST_BASE_URL)
   await expect(page.getByText('Manage your pet daycarepets with ease')).toBeVisible()
 })
