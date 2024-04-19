d19c04153173145e52a792a0c13f7fb3391a41b7
maczi01 <mateusz.w.twardy@gmail.com>
2024-04-19 15:27:01 +0200

    feat: linting

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -8,56 +8,53 @@ export const PetDetails = () => {
     const { selectedPet } = usePetContext();
 
     return (
-        <section className=' flex flex-col size-full'>
+        <section className=" flex flex-col size-full">
             {!selectedPet ? (
-                    <EmptyView />) :
-                (<>
+                <EmptyView />
+            ) : (
+                <>
                     <TopBar pet={selectedPet} />
                     <OtherInfo selectedPet={selectedPet} />
                     <Notes pet={selectedPet}></Notes>
-                </>)
-
-            }
+                </>
+            )}
         </section>
     );
 };
 
 const EmptyView = () => {
     return (
-        <div className='flex items-center justify-center w-full h-full'>
-            <h2 className='text-3xl font-semibold'>No pet selected</h2>
+        <div className="flex items-center justify-center w-full h-full">
+            <h2 className="text-3xl font-semibold">No pet selected</h2>
         </div>
     );
 };
 
 const TopBar = ({ pet }: { pet: Pet | undefined }) => {
     return (
-        <div className='flex items-center bg-white px-8 py-5 border-b border-black/[0.08]'>
+        <div className="flex items-center bg-white px-8 py-5 border-b border-black/[0.08]">
             <Image
                 src={pet?.imageUrl || ''}
-                alt='Pet image'
+                alt="Pet image"
                 width={75}
                 height={75}
-                className='h-[75px] w-[75px] rounded-full object-cover'
+                className="h-[75px] w-[75px] rounded-full object-cover"
             />
-            <h2 className='text-3xl font-semibold leading-7 ml-5'>
-                {pet?.name || 'Select a pet'}
-            </h2>
+            <h2 className="text-3xl font-semibold leading-7 ml-5">{pet?.name || 'Select a pet'}</h2>
         </div>
-
     );
 };
 
 const OtherInfo = ({ selectedPet }: { selectedPet: Pet | undefined }) => {
     return (
-        <div className='flex justify-around px-5 py-10 text-center'>
-            <div className=''>
-                <h3 className='text-[13px] font-medium uppercase text-zinc-700'>Owner name</h3>
-                <p className='mt-1 text-lg text-zinc-800'>{selectedPet?.ownerName}</p>
+        <div className="flex justify-around px-5 py-10 text-center">
+            <div className="">
+                <h3 className="text-[13px] font-medium uppercase text-zinc-700">Owner name</h3>
+                <p className="mt-1 text-lg text-zinc-800">{selectedPet?.ownerName}</p>
             </div>
-            <div className=''>
-                <h3 className='text-[13px] font-medium uppercase text-zinc-700'>Age</h3>
-                <p className='mt-1 text-lg text-zinc-800'>{selectedPet?.age}</p>
+            <div className="">
+                <h3 className="text-[13px] font-medium uppercase text-zinc-700">Age</h3>
+                <p className="mt-1 text-lg text-zinc-800">{selectedPet?.age}</p>
             </div>
         </div>
     );
@@ -65,7 +62,7 @@ const OtherInfo = ({ selectedPet }: { selectedPet: Pet | undefined }) => {
 
 const Notes = ({ pet }: { pet: Pet | undefined }) => {
     return (
-        <section className='flex-1 bg-white px-7 py-5 rounded-md mb-9 mx-8 border-b border-black/[0.08]'>
+        <section className="flex-1 bg-white px-7 py-5 rounded-md mb-9 mx-8 border-b border-black/[0.08]">
             {pet?.notes}
         </section>
     );
